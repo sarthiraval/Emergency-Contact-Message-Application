9696b91c01b5539297b25c38972587eeebf0b482
Sarthi <ssr20810@gmail.com>
2023-04-22 18:36:50 -0700

    first commit

---
+++ app/src/main/java/com/example/helpme/ContactModel.java
@@ -0,0 +1,58 @@
+package com.example.helpme;
+
+public class ContactModel {
+    private int id;
+    private String phoneNo;
+    private String name;
+
+    
+    public ContactModel(int id, String name, String phoneNo) {
+        this.id = id;
+        this.phoneNo = validate(phoneNo);
+        this.name = name;
+    }
+
+    
+    private String validate(String phone) {
+
+        
+        StringBuilder case1 = new StringBuilder("+91");
+        StringBuilder case2 = new StringBuilder("");
+
+        
+        if (phone.charAt(0) != '+') {
+            for (int i = 0; i < phone.length(); i++) {
+                
+                if (phone.charAt(i) != '-' && phone.charAt(i) != ' ') {
+                    case1.append(phone.charAt(i));
+                }
+            }
+            return case1.toString();
+        } else {
+            for (int i = 0; i < phone.length(); i++) {
+                
+                if (phone.charAt(i) != '-' || phone.charAt(i) != ' ') {
+                    case2.append(phone.charAt(i));
+                }
+            }
+            return case2.toString();
+        }
+
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ app/src/main/java/com/example/helpme/CustomAdapter.java
@@ -0,0 +1,94 @@
+package com.example.helpme;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+import java.util.List;
+
+public class CustomAdapter extends ArrayAdapter<ContactModel> {
+
+    Context context;
+    List<ContactModel> contacts;
+
+    public CustomAdapter(@NonNull Context context, List<ContactModel> contacts) {
+        super(context, 0, contacts);
+        this.context = context;
+        this.contacts = contacts;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        
+        
+        DbHelper db = new DbHelper(context);
+
+        
+        ContactModel c = getItem(position);
+
+        
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_user, parent, false);
+        }
+
+        LinearLayout linearLayout = (LinearLayout) convertView.findViewById(R.id.linear);
+
+        
+        TextView tvName = (TextView) convertView.findViewById(R.id.tvName);
+        TextView tvPhone = (TextView) convertView.findViewById(R.id.tvPhone);
+
+        
+        
+        tvName.setText(c.getName());
+        tvPhone.setText(c.getPhoneNo());
+
+        linearLayout.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                
+                new MaterialAlertDialogBuilder(context)
+                        .setTitle("Remove Contact")
+                        .setMessage("Are you sure want to remove this contact?")
+                        .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                
+                                db.deleteContact(c);
+                                
+                                contacts.remove(c);
+                                
+                                notifyDataSetChanged();
+                                Toast.makeText(context, "Contact removed!", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        })
+                        .show();
+                return false;
+            }
+        });
+        
+        return convertView;
+    }
+
+    
+    public void refresh(List<ContactModel> list) {
+        contacts.clear();
+        contacts.addAll(list);
+        notifyDataSetChanged();
+    }
+}
